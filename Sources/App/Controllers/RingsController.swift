adc788d11534027d0aa8351b9795ab76e8a8383f
Débora <d.antunes@kigroup.de>
2022-11-15 14:10:23 +0000

    feat(ring-languages): update ring languages

--- Sources/App/Controllers/RingsController.swift
+++ Sources/App/Controllers/RingsController.swift
@@ -1,11 +1,13 @@
 import Vapor
+import Fluent
+import Foundation
 
 struct RingsController: RouteCollection {
   func boot(routes: RoutesBuilder) throws {
     let ringsRoutes = routes.grouped("api", "rings")
     ringsRoutes.get(use: getAllHandler)
     ringsRoutes.get(":ringID", use: getHandler)
-    ringsRoutes.get(":ringID", "languages", use: getLanguagesHendler)
+    ringsRoutes.get(":ringID", "languages", use: getLanguagesHandler)
 
     let tokenAuthMiddleware = Token.authenticator()
     let guardAuthMiddleware = User.guardMiddleware()
@@ -16,10 +18,11 @@ struct RingsController: RouteCollection {
     protected.post(use: createHandler)
     protected.put(":ringID", use: updateHandler)
     protected.delete(":ringID", use: deleteHandler)
+    protected.put(":ringID", "languages", use: updateLanguagesHandler)
   }
 
   func getAllHandler(_ req: Request) throws -> EventLoopFuture<[Ring]> {
-    Ring.query(on: req.db).all()
+    Ring.query(on: req.db).with(\.$languages).all()
   }
 
   func getHandler(_ req: Request) throws -> EventLoopFuture<Ring> {
@@ -54,11 +57,34 @@ struct RingsController: RouteCollection {
       }
   }
 
-  func getLanguagesHendler(_ req: Request) throws -> EventLoopFuture<[Language]> {
+  func getLanguagesHandler(_ req: Request) throws -> EventLoopFuture<[Language]> {
     Ring.find(req.parameters.get("ringID"), on: req.db)
       .unwrap(or: Abort(.notFound))
       .flatMap { ring in
         ring.$languages.get(on: req.db)
       }
+  }
+
+  func updateLanguagesHandler(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
+    guard let id = req.parameters.get("ringID", as: UUID.self) else {
+      throw Abort(.internalServerError)
+    }
+
+    let languages = try req.content.decode([String].self).map {
+      Language(name: $0, ringID: id)
+    }
+
+    let removeLanguages = Language.query(on: req.db)
+      .filter(\Language.$ring.$id == id)
+      .delete()
+
+    let createLanguages = Ring
+      .find(id, on: req.db)
+      .unwrap(or: Abort(.notFound))
+      .flatMap { ring in
+        ring.$languages.create(languages, on: req.db)
+      }
+
+    return removeLanguages.and(createLanguages).transform(to: .noContent)
   }
 }
